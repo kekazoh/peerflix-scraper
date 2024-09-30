4f2f6b150059e524bef8da4d1ce0c1b96bc7f7fb
Germán Castro <germancastromontes@gmail.com>
2024-09-30 19:08:43 +0200

    feat: add logger to Kafka producer and consumer classes

--- src/events/producer.ts
+++ src/events/producer.ts
@@ -1,4 +1,5 @@
 import { Kafka, Producer, ProducerRecord } from 'kafkajs';
+import logger from '../lib/logger';
 
 export class EventsProducer {
   private kafka: Kafka;
@@ -6,16 +7,24 @@ export class EventsProducer {
   private producer: Producer;
 
   constructor() {
+    logger.debug('Initializing EventsProducer');
     const KAFKA_BROKERS = process.env.KAFKA ? process.env.KAFKA.split(',') : ['localhost:19092'];
-    this.kafka = new Kafka({ 
+    logger.debug({ KAFKA_BROKERS }, 'Kafka brokers configured');
+
+    this.kafka = new Kafka({
       brokers: KAFKA_BROKERS,
       clientId: 'peerflix-scraper',
     });
+    logger.debug('Kafka instance created');
+
     this.producer = this.kafka.producer();
+    logger.debug('Kafka producer created');
   }
 
   async connect(): Promise<void> {
+    logger.debug('Connecting producer');
     await this.producer.connect();
+    logger.info('Producer connected');
   }
 
   async produce(topic: string, message: string, key: string): Promise<void> {
@@ -23,10 +32,14 @@ export class EventsProducer {
       topic,
       messages: [{ value: message, key }],
     };
+    logger.debug({ topic, key, message }, 'Producing message');
     await this.producer.send(record);
+    logger.debug('Message produced successfully');
   }
 
   async disconnect(): Promise<void> {
+    logger.debug('Disconnecting producer');
     await this.producer.disconnect();
+    logger.info('Producer disconnected');
   }
 }
