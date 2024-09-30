8915e4f475a0620e2277369c6ccd3831e114d469
Germán Castro <germancastromontes@gmail.com>
2024-09-30 19:25:27 +0200

    feat: add logging to Scraper base class

--- src/scrapers/scraper.ts
+++ src/scrapers/scraper.ts
@@ -1,6 +1,7 @@
 import { EventsConsumer } from '../events/consumer';
 import { EventsProducer } from '../events/producer';
 import { CheckerResponse, Magnet, MagnetData, ScraperRequest, SeedsNPeers, Torrent } from '../interfaces';
+import logger from '../lib/logger';
 import { getParamFromMagnet } from '../lib/strings';
 import { decodeTorrentFile, magnetURIEncode } from '../lib/torrent';
 
@@ -26,34 +27,47 @@ abstract class Scraper {
     this.consumer = new EventsConsumer(`${platform}-scraper`);
     this.producer = new EventsProducer();
     this.trackers = '';
+    this.logger.info({ platform: this.platform }, 'Scraper initialized');
+  }
+
+  /**
+   * Returns a child logger instance with the platform context.
+   * This logger can be used for platform-specific logging.
+   * @returns A child logger instance with platform context
+   */
+  protected get logger(): typeof logger {
+    return logger.child({ platform: this.platform });
   }
 
   private async getTrackerList(): Promise<string> {
+    this.logger.debug('Fetching tracker list');
     const res = await fetch('https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt');
     const text = await res.text();
     const trackers = text.split('\n').filter((line) => line.length > 0);
+    this.logger.info({ trackers }, 'Tracker list fetched');
     return '&tr=' + trackers.join('&tr=');
   }
 
   public async run(): Promise<void> {
     this.trackers = await this.getTrackerList();
-    console.log('Fetched trackers', this.trackers);
     // Connect to Kafka
     await this.producer.connect();
     // Start consuming messages
     await this.consumer.consume('scrapingRequests', async (message: ScraperRequest) => {
+      this.logger.info({ message }, 'Received message');
       try {
         // Process the message
         const magnets = await this.processMessage(message);
+        this.logger.debug({ magnets }, 'Magnets processed');
         for (const magnet of magnets) {
           const magnetMessage = { ...magnet, cacheId: message.cacheId };
-          console.log('Checking seeds and peers for magnet', magnetMessage);
+          this.logger.debug({ magnetMessage }, 'Checking seeds and peers for magnet');
           const seeds = await this.getSeedersAndPeers(magnet.magnetUrl);
-          console.log('Producing magnet', { ...magnetMessage, ...seeds });
+          this.logger.debug({ ...magnetMessage, ...seeds }, 'Producing magnet');
           await this.producer.produce('magnets', JSON.stringify({ ...magnetMessage, ...seeds }), message.cacheId);
         }
-      } catch (err) {
-        console.error('Error processing message', err);
+      } catch (error) {
+        this.logger.error({ error }, 'Error processing message');
       }
     });
   }
@@ -61,14 +75,18 @@ abstract class Scraper {
   protected abstract processMessage(message: ScraperRequest): Promise<Magnet[]>;
 
   async getMagnetFromTorrentUrl(fullUrl: string, referer?: string) : Promise<MagnetData> {
+    this.logger.debug({ fullUrl, referer }, 'Getting magnet from torrent URL');
     const url = new URL(fullUrl);
     const headers = new Headers();
     if (referer) headers.append('referer', referer);
     const torrentfile = await fetch(url, {
       headers,
     });
-    const torrdata = await torrentfile.arrayBuffer();
-    return this.getMagnetFromRawTorrent(Buffer.from(torrdata));
+    const torrentData = await torrentfile.arrayBuffer();
+    this.logger.debug({ torrentData }, 'Torrent data fetched');
+    const magnetData = await this.getMagnetFromRawTorrent(Buffer.from(torrentData));
+    this.logger.debug({ magnetData }, 'Magnet data fetched');
+    return magnetData;
   }
 
   async getMagnetFromRawTorrent(rawTorrentFile: Buffer): Promise<MagnetData> {
@@ -83,6 +101,7 @@ abstract class Scraper {
   }
 
   async getSeedersAndPeers(magnet: string): Promise<SeedsNPeers> {
+    this.logger.debug({ magnet }, 'Getting seeders and peers');
     const queryParams = new URLSearchParams({
       magnet: magnet + this.trackers,
     });
@@ -92,7 +111,7 @@ abstract class Scraper {
     const data = await res.json() as CheckerResponse;
     const { extra } = data;
     if (data.error?.code) {
-      console.warn('Error checking seeds and peers', data.error.message);
+      this.logger.warn({ error: data.error.message }, 'Error checking seeds and peers');
       return {};
     }
     if (res.status === 200 && extra.length) {
@@ -104,9 +123,11 @@ abstract class Scraper {
           curr: { seeds: number, peers: number }) =>
           ( acc.seeds > curr.seeds ? acc : curr),
         { seeds: 0, peers: 0 });
-      return { seed: maxSeed.seeds, peer: maxSeed.peers };
+      const seedsNPeers = { seed: maxSeed.seeds, peer: maxSeed.peers };
+      this.logger.debug({ seedsNPeers }, 'Seeds and peers fetched');
+      return seedsNPeers;
     }
-    console.warn('Error checking seeds and peers', data);
+    this.logger.warn({ data }, 'Error checking seeds and peers');
     return {};
   }
 
@@ -115,14 +136,14 @@ abstract class Scraper {
       const dTorrent = await (await fetch(
         `${MAGNET2TORRENT_URL}${magnet}`)).json() as Magnet2Torrent;
       if ('url' in dTorrent) {
-        console.log('Found torrent file', dTorrent.url);
+        this.logger.debug({ url: dTorrent.url }, 'Found torrent file');
         const torrFileUrl = dTorrent.url.split('<')[0];
         const magnetInfo = await this.getMagnetFromTorrentUrl(torrFileUrl);
         if (magnetInfo) return magnetInfo;
       }
       return null;
     } catch (error) {
-      console.log('Error getting torrent from magnet', error);
+      this.logger.error({ error }, 'Error getting torrent from magnet');
       return null;
     }
   }
